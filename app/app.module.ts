60ef3729dfd369849b388babb5d41c915d59ac17
edrebg@gmail.com <Ed Gleeck>
2017-01-01 20:27:16 -0500

    Added basic bug module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BugModule } from './bugs/bug.module';
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, BugModule ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
