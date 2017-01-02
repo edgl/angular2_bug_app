96904b72dd989bb29adae068071bcfdd6bee1fdc
edrebg@gmail.com <Ed Gleeck>
2017-01-02 14:46:35 -0500

    core module setup done

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BugModule } from './bugs/bug.module';
+import { CoreModule } from './core/core.module';
 
 // component
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-    imports: [BrowserModule, BugModule, AppRoutingModule],
+    imports: [BrowserModule, BugModule, AppRoutingModule, CoreModule.forRoot()],
     declarations: [AppComponent, NavbarComponent],
     bootstrap: [AppComponent]
 })
