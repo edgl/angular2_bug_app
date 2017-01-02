5764c4991db2f636551f37318eb0447fe21cc820
edrebg@gmail.com <Ed Gleeck>
2017-01-01 22:05:05 -0500

    Finished routing with wildcard redirection

--- app/bugs/bug-routing.module.js
+++ app/bugs/bug-routing.module.js
@@ -19,7 +19,8 @@ var BugRoutingModule = (function () {
             imports: [
                 router_1.RouterModule.forChild([
                     { path: '', redirectTo: 'bugs', pathMatch: 'full' },
-                    { path: 'bugs', component: bug_list_component_1.BugListComponent }
+                    { path: 'bugs', component: bug_list_component_1.BugListComponent },
+                    { path: '**', redirectTo: 'bugs' }
                 ])
             ],
             exports: [router_1.RouterModule]
