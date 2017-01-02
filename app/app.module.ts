5eae4ee428389982d808f7f3ccb88ddf4384584f
edrebg@gmail.com <Ed Gleeck>
2017-01-01 22:21:02 -0500

    started navbar

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,17 @@
+// Module
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BugModule } from './bugs/bug.module';
 
+// component
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
-    imports: [ BrowserModule, BugModule, AppRoutingModule ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    imports: [BrowserModule, BugModule, AppRoutingModule],
+    declarations: [AppComponent, NavbarComponent],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
